21ef5a29a36e7336248e9bce27925491aa4da3fa
xNavi <miko1.religa@gmail.com>
2023-11-27 18:19:16 +0100

    change

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import date
 from flask import Flask, render_template, redirect, url_for, flash, request, abort
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
-#from flask_gravatar import Gravatar
+from flask_gravatar import Gravatar
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, login_required
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
@@ -33,14 +33,14 @@ ckeditor = CKEditor(app)
 Bootstrap5(app)
 
 
-#gravatar = Gravatar(app,
-#                    size=100,
-#                    rating='g',
-#                    default='retro',
-#                    force_default=False,
-#                    force_lower=False,
-#                    use_ssl=False,
-#                    base_url=None)
+gravatar = Gravatar(app,
+                    size=100,
+                    rating='g',
+                    default='retro',
+                    force_default=False,
+                    force_lower=False,
+                    use_ssl=False,
+                    base_url=None)
 
 
 # TODO: Configure Flask-Login
