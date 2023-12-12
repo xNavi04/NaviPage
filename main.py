8197f28816569cbd0789e45b47d6c5d4435f3edb
xNavi <miko.religa@gmail.com>
2023-12-12 12:47:29 +0100

    change

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ gravatar = Gravatar(app,
 
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "postgresql:///posts.db")
 db = SQLAlchemy()
 db.init_app(app)
 
