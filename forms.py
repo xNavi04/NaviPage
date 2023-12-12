5ca02e62d42f8f69fc2528bc2751d32cd719cefb
xNavi <miko.religa@gmail.com>
2023-12-12 11:46:54 +0100

    change

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, EmailField, PasswordField
-from wtforms.validators import DataRequired, URL, Email, email
+from wtforms.validators import DataRequired, URL, Email
 from flask_ckeditor import CKEditorField
 
 class CreatePostForm(FlaskForm):
